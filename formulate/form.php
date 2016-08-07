c011aa956d7b91f5ebbdff9d3bb4bbade23f103e
Arnon Erba <arnon@arnonerba.com>
2016-08-07 11:51:03 -0700

    refactored to allow for reordering of form elements

--- formulate/form.php
+++ formulate/form.php
@@ -5,8 +5,25 @@
 function fcf_build() {
 	global $configured;
 	if ( $configured ) {
-		echo '<script src="https://www.google.com/recaptcha/api.js"></script>
-		<form class="fcf-card" action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">
+
+		$fcfreCAPTCHAjs = '<script src="https://www.google.com/recaptcha/api.js"></script>';
+
+		$fcfContactNameField = '<input type="text" id="fcf-contact-name" class="fcf-textfield" name="fcf_contact_name" pattern="[a-zA-Z0-9\. ]+" value="' . ( isset( $_POST["fcf_contact_name"] ) ? esc_attr( $_POST["fcf_contact_name"] ) : '' ) . '">';
+		$fcfContactNameLabel = '<label class="fcf-textfield-label" for="fcf-contact-name">Your Name</label>';
+
+		$fcfPhoneNumberField = '<input type="tel" id="fcf-phone-number" class="fcf-textfield" name="fcf_phone_number" pattern="[0-9\(\)\-\+\* ]+" value="' . ( isset( $_POST["fcf_phone_number"] ) ? esc_attr( $_POST["fcf_phone_number"] ) : '' ) . '">';
+		$fcfPhoneNumberLabel = '<label class="fcf-textfield-label" for="fcf-phone-number">Phone Number (optional)</label>';
+
+		$fcfContactEmailField = '<input type="email" id="fcf-contact-email" class="fcf-textfield" name="fcf_contact_email" value="' . ( isset( $_POST["fcf_contact_email"] ) ? esc_attr( $_POST["fcf_contact_email"] ) : '' ) . '">';
+		$fcfContactEmailLabel = '<label class="fcf-textfield-label" for="fcf-contact-email">Email Address</label>';
+
+		$fcfContactMessageField = '<textarea id="fcf-contact-message" class="fcf-textfield textarea" type="text" rows= "5" name="fcf_contact_message">' . ( isset( $_POST["fcf_contact_message"] ) ? esc_attr( $_POST["fcf_contact_message"] ) : '' ) . '</textarea>';
+		$fcfContactMessageLabel = '<label class="fcf-textfield-label" for="fcf-contact-message">Message</label>';
+
+		echo $fcfreCAPTCHAjs;
+
+		echo '<form class="fcf-card" action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">
+
 			<div class="fcf-card-title">';
 			if ( get_option( 'fcf_form_title' ) ) {
 				$title = sanitize_text_field( get_option( 'fcf_form_title' ) );
@@ -14,20 +31,48 @@ function fcf_build() {
 				$title = 'Contact Us';
 			}
 			echo $title . '</div>
-			<div class="fcf-card-text">
-				<input type="text" id="fcf-contact-name" class="fcf-textfield" name="fcf_contact_name" pattern="[a-zA-Z0-9\. ]+" value="' . ( isset( $_POST["fcf_contact_name"] ) ? esc_attr( $_POST["fcf_contact_name"] ) : '' ) . '">
-				<label class="fcf-textfield-label" for="fcf-contact-name">Your Name</label>';
+
+			<div class="fcf-card-text">';
+
+				global $theme;
+				if ( $theme == 3 ) {
+					echo $fcfContactNameLabel;
+					echo $fcfContactNameField;
+				} else {
+					echo $fcfContactNameField;
+					echo $fcfContactNameLabel;
+				}
+
 				global $displayTel;
 				if ( $displayTel == 1 ) {
-					echo '<input type="tel" id="fcf-phone-number" class="fcf-textfield" name="fcf_phone_number" pattern="[0-9\(\)\-\+\* ]+" value="' . ( isset( $_POST["fcf_phone_number"] ) ? esc_attr( $_POST["fcf_phone_number"] ) : '' ) . '">
-					<label class="fcf-textfield-label" for="fcf-phone-number">Phone Number (optional)</label>';
+					global $theme;
+					if ( $theme == 3 ) {
+						echo $fcfPhoneNumberLabel;
+						echo $fcfPhoneNumberField;
+					} else {
+						echo $fcfPhoneNumberField;
+						echo $fcfPhoneNumberLabel;
+					}
 				}
-				echo '<input type="email" id="fcf-contact-email" class="fcf-textfield" name="fcf_contact_email" value="' . ( isset( $_POST["fcf_contact_email"] ) ? esc_attr( $_POST["fcf_contact_email"] ) : '' ) . '">
-				<label class="fcf-textfield-label" for="fcf-contact-email">Email Address</label>
-				<textarea id="fcf-contact-message" class="fcf-textfield textarea" type="text" rows= "5" name="fcf_contact_message">' . ( isset( $_POST["fcf_contact_message"] ) ? esc_attr( $_POST["fcf_contact_message"] ) : '' ) . '</textarea>
-				<label class="fcf-textfield-label" for="fcf-contact-message">Message</label>
-				
-			</div>
+
+				if ( $theme == 3 ) {
+					echo $fcfContactEmailLabel;
+					echo $fcfContactEmailField;
+				} else {
+					echo $fcfContactEmailField;
+					echo $fcfContactEmailLabel;
+				}
+
+				if ( $theme == 3 ) {
+					echo $fcfContactMessageLabel;
+					echo $fcfContactMessageField;
+				} else {
+					echo $fcfContactMessageField;
+					echo $fcfContactMessageLabel;
+				}
+
+			echo '</div>
+
 			<div id="fcf-recaptcha">
 				<div class="g-recaptcha"';
 				global $theme;
@@ -38,9 +83,11 @@ function fcf_build() {
 				}
 				echo ' data-sitekey="' . sanitize_text_field( get_option( 'fcf_recaptcha_sitekey' ) ) . '"></div>
 			</div>
+
 			<div class="fcf-card-actions">
 				<button id="fcf_submitted" type="submit" class="fcf-button" name="fcf_submitted">Send</button>
 			</div>
+
 		</form>';
 	}
 }
